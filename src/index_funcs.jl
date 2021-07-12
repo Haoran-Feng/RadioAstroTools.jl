d7b84dda65330735ba8fbb2b0c56dcbb5f8432ce
Haoran-Feng <hrfeng@pmo.ac.cn>
2021-07-12 14:48:18 +0800

    add Colon support in index_funcs.jl

--- src/index_funcs.jl
+++ src/index_funcs.jl
@@ -1,8 +1,8 @@
 
 # index functions
-Base.getindex(cube::AbstractSpectralCube, i::Integer, j::Integer, k::Integer) = cube.data[i, j, k]
+Base.getindex(cube::AbstractSpectralCube, i::Union{Colon, Integer}, j::Union{Colon, Integer}, k::Union{Colon, Integer}) = cube.data[i, j, k]
 function Base.getindex(cube::AbstractSpectralCube, i::AbstractFloat, j::AbstractFloat, k::Colon)
-    ii = MapAndCube.closest_index(cube.xaxis, i)
-    jj = MapAndCube.closest_index(cube.yaxis, j)
+    ii = closest_index(cube.xaxis, i)
+    jj = closest_index(cube.yaxis, j)
     return cube.data[ii, jj, :]
 end
